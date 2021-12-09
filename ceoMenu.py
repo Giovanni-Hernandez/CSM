4c7946562552d7c299800c581946552d9bef8910
Frunulunuuuu <69992683+marcorh2000@users.noreply.github.com>
2021-12-08 22:03:25 -0600

    Create ceoMenu.py

---
+++ ceoMenu.py
@@ -0,0 +1,31 @@
+from os import system
+import sys
+
+def ceoPrincipalMenu():
+
+    option = 0
+
+    while (option != 4):
+        system('cls')
+        print('C E O   M E N U\n')
+        print('1. Choose a document')
+        print('2. Verify signatures')
+        print('3. Delete a document')
+        print('4. Exit app')
+        print('\nSelect an option: ', end='')
+
+        option = int(input())
+
+        if(option == 1):
+            
+
+        if(option == 2):
+            input()
+
+        if(option == 3)
+            input()
+
+def chooseADocument():
+    input()
+        
+ceoPrincipalMenu()   
