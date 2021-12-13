55cc50830592d3c75cbd8a742fd597b3c27c4c1a
Frunulunuuuu <69992683+marcorh2000@users.noreply.github.com>
2021-12-13 02:06:20 -0600

    CEO encryption

--- ceoMenu.py
+++ ceoMenu.py
@@ -1,5 +1,14 @@
 from os import system
 import sys
+import filesManagement as fm
+import rsa2048 as rsa
+import aesCBC as aes
+
+ceoRoute = 'ceo/'
+privateFolder = 'private/'
+ceoDirectivesRoute = 'ceo/directives/'
+mode = 0o666
+
 
 def ceoPrincipalMenu():
 
@@ -17,15 +26,65 @@ def ceoPrincipalMenu():
         option = int(input())
 
         if(option == 1):
-            
+            chooseADocument()
 
         if(option == 2):
             input()
 
-        if(option == 3)
+        if(option == 3):
             input()
 
+
 def chooseADocument():
-    input()
-        
-ceoPrincipalMenu()   
+
+    folder = ceoRoute + privateFolder + 'documents/'
+
+    # Available documents are shown
+    listOfFiles = fm.listFiles(folder)
+
+    system('cls')
+
+    i = 1
+
+    print("A V A I L A B L E   D O C U M E N T S\n")
+
+    for file in listOfFiles:
+        print("" + str(i) + ". " + file)
+        i = i + 1
+
+    fileSelected = int(input("\nChoose a file: "))
+    filename = listOfFiles[fileSelected - 1]
+
+    # Bytes from file are read
+    contentFile = fm.readFile(folder, filename, '')
+
+    # File is ciphered for all directives
+    listOfDirectives = fm.listDir(ceoDirectivesRoute)
+
+    # Getting CEO's private key
+    CEOPrivKey = rsa.readRSAPrivateKey(ceoRoute + 'private/', 'CEOpriv')
+
+    # Encryption is done as many times as there are directives
+    for directive in listOfDirectives:
+        # Reading the director's encrypted AES key file
+        dirEncAESKey = fm.readFile(
+            ceoDirectivesRoute + directive + '/', 'encryptedKey', '.aes')
+
+        # Decrypting director's AES Key
+        decAESKey = rsa.decryptRSA(dirEncAESKey, CEOPrivKey)
+
+        # Encrypting file using directive's AES key
+        encContent, iv = aes.encryptAES(decAESKey, contentFile)
+
+        # Document directory is created
+        fm.createDir('directives/' + directive + '/documents/', filename)
+        fileDir = 'directives/' + directive + '/documents/' + filename + '/'
+
+        # Sending ecrypted document and IV to directive
+        fm.saveFile(fileDir, 'encryptedFile', '.enc', contentFile)
+        fm.saveFile(fileDir, 'iv', '.csv', contentFile)
+
+    print("Documents have been sent to each directive")
+
+
+ceoPrincipalMenu()
--- filesManagement.py
+++ filesManagement.py
@@ -2,13 +2,17 @@ import base64
 import subprocess
 import os
 
-#Saves a file in a route with specific extension using base 64
+# Saves a file in a route with specific extension using base 64
+
+
 def savefile64(route, filename, extension, contentBytes):
     f = open(route + filename + extension, 'wb')
     f.write(base64.b64encode(contentBytes))
     f.close()
 
-#Reads bytes from file in base 64
+# Reads bytes from file in base 64
+
+
 def readFile64(route, filename, extension):
     f = open(route + filename + extension, 'rb')
     content = base64.b64decode(f.read())
@@ -16,33 +20,49 @@ def readFile64(route, filename, extension):
 
     return content
 
-#Reads bytes from file
+# Reads bytes from file
+
+
 def readFile(route, filename, extension):
     f = open(route + filename + extension, 'rb')
     content = f.read()
     f.close()
     return content
 
-#Saves a file in a route with specific extension
+# Saves a file in a route with specific extension
+
+
 def saveFile(route, filename, extension, contentBytes):
     f = open(route + filename + extension, 'wb')
     f.write(contentBytes)
     f.close
 
 
-#Open a file in any format
+# Open a file in any format
 def openFile(route, filename, extension):
-    direccion = os.path.abspath(os.getcwd()) + "/" + route + filename + extension
-    subprocess.Popen(direccion, shell = True)
+    direccion = os.path.abspath(os.getcwd()) + "/" + \
+        route + filename + extension
+    subprocess.Popen(direccion, shell=True)
 
-#Return a list of all documents that are in a carpet
+# Return a list of all documents that are in a carpet
+
+
 def listFiles(route):
     return os.listdir(route)
 
-#Deelet a file that you select
+# Delete a file that you select
+
+
 def deleteFile(route):
     os.remove(route)
 
-#savefile('', 'prueba', '.txt', b'Comida')
-#print(str(readFile('', 'prueba', '.txt')))
+# List of all directories inside a directory
 
+
+def listDir(route):
+    return [d for d in os.listdir(route) if os.path.isdir(os.path.join(route, d))]
+
+
+# Create a directory
+def createDir(route, name):
+    os.mkdir(route + name)
